7a6e996e327d1cd203007a75a199f1b0c2c8911b
Shih-Po Wang <shihpo@gmail.com>
2018-11-21 08:30:39 +0000

    add interactive mode using asciimatics

---
+++ src/Models.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python2.7
+#-*- coding: utf-8 -*-
+
+import datetime
+import sqlite3
+
+class MessageModel(object):
+    def __init__(self, RC, config):
+        self._config = config
+        self._RC = RC
+        self._db = sqlite3.connect(':memory:')
+        self._db.row_factory = sqlite3.Row
+
+        # Create the basic contact table.
+        self._db.cursor().execute('''
+            CREATE TABLE messages(
+                id INTEGER PRIMARY KEY,
+                messageId TEXT unique,
+                roomId TEXT,
+                timestamp TEXT,
+                author TEXT,
+                text TEXT)
+        ''')
+        self._db.commit()
+
+    def add(self, message):
+        try:
+            self._db.cursor().execute('''
+                INSERT INTO messages(messageId, roomId, timestamp, text, author)
+                VALUES(:_id, :rid, :ts, :msg, :author)''',
+                { '_id': message['_id'],
+                  'rid': message['rid'],
+                  'ts': message['ts'],
+                  'msg': message['msg'],
+                  'author': message['u']['name']})
+            self._db.commit()
+        except sqlite3.IntegrityError:
+            pass
+
+    def get_summary(self, channelId = None):
+        if channelId is None:
+            return [ ('None', 1)]
+
+        return self._db.cursor().execute(
+            "SELECT messageId, id from messages WHERE roomId=:id", {"id": channelId}).fetchall()
+
+    def _retrieve(self, channelId, userId = None, mention = False):
+        messages = []
+
+        now = datetime.datetime.now()
+        oldtime = now + datetime.timedelta(days=int(self._config['period']))
+        nowStr = '{}Z'.format(now.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3])
+        oldTimeStr = '{}Z'.format(oldtime.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3])
+
+        data = self._RC.channels_history(channelId, nowStr, oldTimeStr, 100)
+
+        if mention is False:
+            return data['messages']
+
+        for message in data['messages']:
+            if 'mentions' in message:
+                for mention in message['mentions']:
+
+                    if userId is None:
+                        self.add(message)
+                    elif mention['_id'] == userId:
+                        self.add(message)
+
+class ChannelModel(object):
+    def __init__(self, messageModel, RC):
+        self._RC = RC
+        self._messages = messageModel
+        self._db = sqlite3.connect(':memory:')
+        self._db.row_factory = sqlite3.Row
+
+        # Create the basic contact table.
+        self._db.cursor().execute('''
+            CREATE TABLE channels(
+                id INTEGER PRIMARY KEY,
+                roomId TEXT,
+                name TEXT,
+                unique(roomId, name))
+        ''')
+        self._db.commit()
+
+        # Current contact when editing.
+        self.current_id = None
+
+    def get_summary(self):
+        return self._db.cursor().execute(
+            "SELECT name, roomId from channels").fetchall()
+
+    def add(self, channel):
+        try:
+            self._db.cursor().execute('''
+                INSERT INTO channels(roomId, name)
+                VALUES(:_id, :name)''',
+                channel)
+            self._db.commit()
+        except sqlite3.IntegrityError:
+            pass
+
+    def _retrieve(self):
+        data = self._RC.channels_list_joined()
+        user_info = self._RC.users_info()
+
+        userId = user_info['user']['_id']
+
+        for channel in data['channels']:
+            self.add(channel)
+            self._messages._retrieve(channel['_id'], userId, mention = True)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,77 @@
 #!/usr/bin/env python2.7
 #-*- coding: utf-8 -*-
 
+import sys
 import getpass
 import json
 import datetime
+
+from optparse import OptionParser
 from rocketchat.api import API
+
+from asciimatics.widgets import Frame
+from asciimatics.widgets import ListBox
+from asciimatics.widgets import Layout
+from asciimatics.widgets import Button
+from asciimatics.widgets import Widget
+
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
+from time import sleep
+
+from Models import ChannelModel
+from Models import MessageModel
 
 RC = None
 config = None
+
+class View(Frame):
+    def __init__(self, screen, channelModel, messageModel):
+        super(View, self).__init__(screen,
+                screen.height, screen.width,
+                on_load=self._retrieve,
+                title="Channel List")
+        self._channelModel = channelModel
+        self._messageModel = messageModel
+
+        self._list_view = ListBox(
+            Widget.FILL_FRAME,
+            channelModel.get_summary(),
+            name="channels",
+            add_scroll_bar=True,
+            on_change=self._on_channel_pick)
+
+        self._messageListView = ListBox(
+            Widget.FILL_FRAME,
+            messageModel.get_summary(),
+            name="messages",
+            add_scroll_bar=True,
+            on_change=self._on_pick)
+
+        layout = Layout([1, 8, 1])
+        self.add_layout(layout)
+        layout.add_widget(self._list_view, 0)
+        layout.add_widget(self._messageListView, 1)
+        layout.add_widget(Button("Refresh", self._retrieve), 2)
+        layout.add_widget(Button("Export to CSV", self._quit), 2)
+        layout.add_widget(Button("Quit", self._quit), 2)
+        self.fix()
+
+    def _on_channel_pick(self):
+        channelId = self._list_view.value
+        self._messageListView.options = self._messageModel.get_summary(channelId)
+
+    def _on_pick(self):
+        pass
+
+    def _retrieve(self):
+        self._channelModel._retrieve()
+        self._list_view.options = self._channelModel.get_summary()
+
+    @staticmethod
+    def _quit():
+        raise StopApplication("User pressed quit")
 
 def loadJSONConfig(path = './config.json'):
     global config;
@@ -70,9 +134,39 @@ def doLogin():
     password = getpass.getpass("Please input password：")
     data = RC.login(user, password)
 
-if __name__ == '__main__':
-    config = loadJSONConfig()
-    RC = API(config['rocketchat'])
-    print 'Rocket.Chat:', RC.info()['info']['version']
+config = loadJSONConfig()
+RC = API(config['rocketchat'])
+messages = MessageModel(RC, config)
+channels = ChannelModel(messages, RC)
+
+def frame(screen):
+    scenes = [
+        Scene([View(screen,
+                    channelModel = channels,
+                    messageModel = messages)], -1, name = 'Main')
+    ]
+    screen.play(scenes)
+
+def interactiveMode():
+    while True:
+        try:
+            Screen.wrapper(frame)
+            sys.exit(0)
+        except ResizeScreenError:
+            pass
+
+if __name__ == "__main__":
+    parser = OptionParser()
+
+    # options settings
+    parser.add_option("-I", "--interactive", default=False,
+        action="store_true", dest = "interactive",
+        help = "enable interactive mode")
+
+    (options, args) = parser.parse_args()
+
     doLogin()
-    getJoinedChannelsMentions()
+    if options.interactive is True:
+        interactiveMode()
+    else:
+        getJoinedChannelsMentions()
